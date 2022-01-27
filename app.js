e390d937ff8db34a81760bfcd5a583782b6e650e
yingzhuo <yingzhuo1994@gmail.com>
2022-01-27 13:30:34 -0600

    Campground New & Create

--- app.js
+++ app.js
@@ -20,19 +20,32 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded( { extended: true }))
+
 app.get('/', (req, res) => {
-    res.render('home')
-})
+    res.render('home');
+});
 
 app.get('/campgrounds', async(req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds })
+});
+
+app.get('/campgrounds/new', async(req, res) => {
+    res.render('campgrounds/new');
+});
+
+app.post('/campgrounds', async(req, res) => {
+    const campground = new Campground(req.body.campground);
+    campground.save();
+    res.redirect(`campgrounds/${campground._id}`)
 })
 
 app.get('/campgrounds/:id', async(req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', { campground});
-})
+});
+
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
