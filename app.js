11b4ea40e7f38c6f1daba630f1598d47133e1a9d
yingzhuo <yingzhuo1994@gmail.com>
2022-02-02 17:49:57 -0600

    More Errors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError')
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 const { cache } = require('ejs');
@@ -42,6 +43,7 @@ app.get('/campgrounds/new', async(req, res) => {
 });
 
 app.post('/campgrounds', catchAsync(async(req, res, next) => {
+    if (!req.body.campground) throw new ExpressError('Invlaid Campground Data', 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`campgrounds/${campground._id}`)
@@ -70,8 +72,13 @@ app.delete('/campgrounds/:id', async (req, res) => {
     res.redirect('/campgrounds');
 })
 
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+})
+
 app.use((err, req, res, next) => {
-    res.send("Oh boy, something went wrong!")
+    const {statusCode = 500, message = "Something went wrong!"} = err;
+    res.status(statusCode).send(message);
 })
 
 app.listen(3000, () => {
