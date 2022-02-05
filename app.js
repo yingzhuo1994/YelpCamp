75f7bbcff29f6a75ca4d89c717091efd6305fdae
yingzhuo <yingzhuo1994@gmail.com>
2022-02-04 23:16:12 -0600

    Deleting Reviews

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 const Review = require('./models/review');
+const review = require('./models/review');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -103,6 +104,13 @@ app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async(req, res) 
     await review.save();
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
+}))
+
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
 }))
 
 app.all('*', (req, res, next) => {
