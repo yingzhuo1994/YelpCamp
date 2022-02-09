d7f3fefedabf666fc42720b38e97f2cb4e28265f
yingzhuo <yingzhuo1994@gmail.com>
2022-02-08 23:49:10 -0600

    serve static assets

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ const reviews = require('./routes/reviews');
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
     // useCreateIndex: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    // useFindAndModify: false
 });
 
 const db = mongoose.connection;
@@ -28,6 +29,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded( { extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/campgrounds', campgrounds)
 app.use('/campgrounds/:id/reviews', reviews)
