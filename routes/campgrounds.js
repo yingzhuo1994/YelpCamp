a13f398c2e03893eb42a63f53beaa467d8788866
yingzhuo <yingzhuo1994@gmail.com>
2022-02-09 23:46:32 -0600

    Flash Success Partial

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -17,7 +17,7 @@ const validateCampground = (req, res, next) => {
 
 router.get('/', async(req, res) => {
     const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', { campgrounds })
+    res.render('campgrounds/index', { campgrounds });
 });
 
 router.get('/new', async(req, res) => {
@@ -29,6 +29,7 @@ router.post('/', validateCampground, catchAsync(async(req, res, next) => {
 
     const campground = new Campground(req.body.campground);
     await campground.save();
+    req.flash('success', 'Successfully made a new campground!');
     res.redirect(`campgrounds/${campground._id}`)
 
 }));
@@ -46,6 +47,7 @@ router.get('/:id/edit', catchAsync(async(req, res) => {
 router.put('/:id', validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+    req.flash('success', 'Successfully updated campground!');
     res.redirect(`/campgrounds/${campground._id}`);
 }));
 
