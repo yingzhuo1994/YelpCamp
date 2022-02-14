5e2e13d377dd2988f3df2f77e8eb13be5f2939c1
yingzhuo <yingzhuo1994@gmail.com>
2022-02-14 13:06:53 -0600

    add an author to campground

--- seeds/index.js
+++ seeds/index.js
@@ -23,6 +23,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '6206fdd685c6bec05e80f77f',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
