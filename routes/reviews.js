b2d481a6ddad7daf9c137cf7a273d4b34531fe37
yingzhuo <yingzhuo1994@gmail.com>
2022-02-14 19:15:31 -0600

    Authorization Middleware

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,22 +3,10 @@ const router = express.Router({ mergeParams: true });
 
 const Campground = require('../models/campground');
 const Review = require('../models/review');
+const { validateReview } = require('../middleware');
 
-const { reviewSchema } = require('../schemas.js');
-
-const ExpressError = require('../utils/ExpressError');
 const catchAsync = require('../utils/catchAsync');
 
-const validateReview = (req, res, next) => {
-    const {error} = reviewSchema.validate(req.body);
-    console.log(error)
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400);
-    } else {
-        next();
-    }    
-}
 
 router.post('/', validateReview, catchAsync(async(req, res) => {
     const campground = await Campground.findById(req.params.id);
