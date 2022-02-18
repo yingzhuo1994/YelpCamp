4a5ad5ae77e93adb78f2031b3ee1f79a4f1f28d7
yingzhuo <yingzhuo1994@gmail.com>
2022-02-18 00:01:01 -0600

    delete images backend

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,5 +1,5 @@
-const { model } = require('mongoose');
 const Campground = require('../models/campground');
+const { cloudinary } = require('../cloudinary');
 
 module.exports.index = async(req, res) => {
     const campgrounds = await Campground.find({});
@@ -47,10 +47,18 @@ module.exports.renderEditForm = async(req, res) => {
 
 module.exports.updateCampground = async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
-    const imgs = req.files.map(f => ({url: f.path, filename: f.filename}));
+    console.log(req.body);
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
     campground.images.push(...imgs);
     await campground.save();
+    if (req.body.deleteImages) {
+        for(let filename of req.body.deleteImages) {
+            await cloudinary.uploader.destroy(filename);
+        }
+        await campground.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages}}}});
+        console.log(campground);
+    }
     req.flash('success', 'Successfully updated campground!');
     res.redirect(`/campgrounds/${campground._id}`);
 }
