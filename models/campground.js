0af5e9747a74e80dc423f0a2a994020c1b3a0677
yingzhuo <yingzhuo1994@gmail.com>
2022-02-18 13:47:33 -0600

    add a thumbnail virtual property

--- models/campground.js
+++ models/campground.js
@@ -2,14 +2,19 @@ const mongoose = require('mongoose');
 const Review = require('./review');
 const Schema = mongoose.Schema;
 
+// https://res.cloudinary.com/ddfyausg1/image/upload/w_300/v1645125811/YelpCamp/ea8oc8vqtwetl0ibhhyi.jpg
+const ImageSchema = new Schema ({
+    url: String,
+    filename: String
+});
+
+ImageSchema.virtual('thumbnail').get(function() {
+    return this.url.replace('/upload', '/upload/w_200');
+});
+
 const CampgroundSchema = new Schema({
     title: String,
-    images: [
-        {
-            url: String,
-            filename: String
-        }
-    ],
+    images: [ImageSchema],
     price: Number,
     description: String,
     location: String,
