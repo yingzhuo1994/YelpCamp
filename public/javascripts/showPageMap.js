d87148fa2c5b72809cc18b0dff156fc22b52ee58
yingzhuo <yingzhuo1994@gmail.com>
2022-02-18 23:38:40 -0600

    center the map on a campground

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,7 +1,11 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-  container: 'map', // container ID
-  style: 'mapbox://styles/mapbox/streets-v11', // style URL
-  center: [-74.5, 40], // starting position [lng, lat]
-  zoom: 8 // starting zoom
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    center: campground.geometry.coordinates, // starting position [lng, lat]
+    zoom: 8 // starting zoom
 });
+
+new mapboxgl.Marker()
+    .setLngLat(campground.geometry.coordinates)
+    .addTo(map)
